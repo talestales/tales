80f26fa4c3750bb3890f7fd3109d58f613cb2c91
tales <talestales@gmail.com>
2020-02-17 18:30:58 -0300

    q cagada

--- exercises/4_2/piram.js
+++ exercises/4_2/piram.js
@@ -1,13 +1,15 @@
-n = 3;
+n = 7;
+m = (n + 1) / 2;
 var i;
 var j;
 var k;
 var linha = '';
-for (i = 0; i < n; i++){
-    for (j = 0; j < n - (i + 1); j++){
+for (i = 0; i < m ; i++){
+    linha = '';
+    for (j = 0; j < m - i; j++){
         linha = linha + " ";
     }
-    for (k = 0; k < i; k++){
+    for (k = j; k < i + m - 1; k++){
         linha = linha + ("*");
     }
     console.log(linha);
